103dda95a678ec3a1646d2636b46d63336a77eee
Rui Gao <rui.ray.gao@gmail.com>
2025-05-03 21:38:47 -0700

    Update RG_Gfit.py

--- RG_Gfit.py
+++ RG_Gfit.py
@@ -1,4 +1,4 @@
-def RG_Gfit(true,pred,residual='Yes'):
+def RG_Gfit(true,pred,num_dicimal,residual='Yes'):
     import numpy as np
     import pylab
     import matplotlib.pyplot as plt
@@ -15,6 +15,7 @@ def RG_Gfit(true,pred,residual='Yes'):
     The input information is:
     param true: the observation data.
     param pred: the predictions gained from models.
+    param num_dicimal: the number of dicimals present on the screen.
     param residual: if the input is 'Yes', residual plot will be presented. Otherwise, no residual plot.
     
     6 statistics (outputs) are:
@@ -23,6 +24,7 @@ def RG_Gfit(true,pred,residual='Yes'):
     rmse: the root mean square error.
     bias: the mean bias computed as the observed minus the predicted.
     r: the Pearson correlation coefficient between the observed and the predicted.
+    p-value: the p-value associated with the Pearson correlation.
     d: the Wilmott's index of agreement.
     '''
     
@@ -46,7 +48,7 @@ def RG_Gfit(true,pred,residual='Yes'):
     # Bias
     bias = statistics.mean(true-pred)
     # Pearson correlation coefficient - r
-    r, _ = pearsonr(true,pred)
+    r, p_value = pearsonr(true,pred)
     # Willmott's index of agreement - d
     tmp_1 = (true-pred)**2
     tmp_1 = tmp_1.sum()
@@ -54,11 +56,15 @@ def RG_Gfit(true,pred,residual='Yes'):
     tmp_2 = (np.abs(true-tmp_2)+np.abs(pred-tmp_2))**2
     d = 1-(tmp_1/tmp_2.sum())
     
-    print('Number of cases: %.0f' % n)
-    print('Root mean square error: %.3f' % rmse)
-    print('Bias: %.3f' % bias)
-    print('Pearsons correlation: %.3f' % r)
-    print('Willmott\'s index of agreement: %.3f' % d)
-    print('Mean square error: %.3f' % mse)
+    # Dynamic string formatting for decimal places
+    fmt = f".{num_dicimal}f"  # Create format string dynamically
     
-    return(n,mse,rmse,bias,r,d)
+    print(f'Number of cases: {n:.0f}')
+    print(f'Root mean square error: {rmse:{fmt}}')
+    print(f'Bias: {bias:{fmt}}')
+    print(f'Pearson correlation: {r:{fmt}}')
+    print(f'P-value: {p_value:.2e}')
+    print(f'Willmott\'s index of agreement: {d:{fmt}}')
+    print(f'Mean square error: {mse:{fmt}}')
+        
+    return(n,mse,rmse,bias,r,p_value,d)
